56ba6aa4bc90e84c364343b3182fb807f7b63812
Kenneth Wong <kennethwork101@gmail.com>
2024-03-31 19:14:53 -0700

    Added pytest support

--- tests/_1_test.py
+++ tests/_1_test.py
@@ -1,21 +1,20 @@
 import pytest
 
-from kwwutils import __version__
+from kwwutils.kwwutils import __version__
 from kwwutils.kwwutils import clock, get_llm, printit
 
 
 @clock
-def test_version():
-    llm_type = "llm"
-    model = "openhermes"
-    temperature = 0.1
-    options = {}
-    options["model"] = model
-    options["temperature"] = temperature
-    options["llm_type"] = llm_type
-    printit("version", __version__)
+def test_version(options, model):
     printit("options", options)
-    assert __version__ == "0.1.0"
+    printit("model", model)
+    assert __version__ == "0.1.2"
+    llm_type = "llm"
+    model = "openermes:latest"
+    temperature = 0.1
+    options["model"] = model
+    options["llm_type"] = llm_type
+    printit("2 options", options)
     llm = get_llm(options)
     printit("llm", llm)
     assert llm.temperature == temperature
